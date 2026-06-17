e50801c933324458acf73af386b3bdf4d7eff37f
i@aarsenev.ru <i@aarsenev.ru>
2026-06-17 14:38:56 +0300

    фикс css экрана на пва 9

--- apps/coach/src/main.tsx
+++ apps/coach/src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { initTheme, initViewport } from "@sport-app/ui";
+import { initTheme } from "@sport-app/ui";
 import App from "./App";
 import "@sport-app/ui/styles.css";
 
 initTheme();
 
-const root = createRoot(document.getElementById("root")!);
-root.render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
-
-initViewport();
